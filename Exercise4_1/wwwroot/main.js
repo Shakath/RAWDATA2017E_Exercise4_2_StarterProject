4ef4d0f6afc7827111e0f5e4a80f2ed014eac7f3
alexander <alexander@ibsen.me>
2017-11-28 11:08:16 +0100

    First Commit

---
+++ Exercise4_1/wwwroot/main.js
@@ -0,0 +1,19 @@
+﻿
+
+(function () {
+	var vm = {
+		title: ko.observable("Hello! :)"),
+
+		changeTitle: function () {
+			console.log(this.title);
+			this.title("Martin is a fuckwit");
+		},
+
+		nextPage: function () {
+			
+		}
+	};
+
+	ko.applyBindings(vm);
+
+})();
